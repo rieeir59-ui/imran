cbfbf1d0f2bfb008856568a8bdf61a5371f3b180
RIE_EIR <rieeir59@gmail.com>
2025-11-17 11:40:03 +0000

    Try fixing this error: `Runtime Error: Error: React is not defined. Erro

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,5 +1,7 @@
+
 'use client';
 
+import * as React from 'react';
 import { useState } from 'react';
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
